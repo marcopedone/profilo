ad2f5a342942941a45a7e48aebcbc55a3b3372ce
Marco Pedone <m.pedone98@gmail.com>
2022-08-23 12:34:31 +0200

    readme

---
+++ README.md
@@ -0,0 +1,13 @@
+#Profilo Utente
+
+Risorse utilizzate:
+- Bootstrap 5 bundle CSS
+- Bootstrap 5 bundle JS
+- Preconnect google Fonts
+- Bootstrap Icons CSS
+- Extra Stile -> style.css
+- Estra JS -> script.js
+
+Rispetto alla form di acquisto qui utilizzo anche bootstrap-icons.
+
+Nella tab ricerca, per i campi comune, tipologia e altre preferenze, vanno mostrati i valori selezionati, con il popup corrispondente, che mostrerà l'insieme completo di campi disponibili e le checkbox pre-selezionate in base al record, non ho imlplementato la funzionalità qui poichè non so come vengono salvate quelle informazioni, se per campi singoli o raggruppati in un'unico campo nel database.
--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-function changed() {
-    document.getElementById('salvabtn').disabled = false;
+function changed(i) {
+    var bottone = "salvabtn" + String(i)
+    document.getElementById(bottone).disabled = false;
 }
+/* DATI UTENTE */
+comunidata = ["Bassano del Grappa", "Romano d'Ezzelino", "Pove del Grappa"]
+tipologiadata = ["Porzione di casa", "Attico", "Mansarda", "Porzione di villa"]
+altreprefdata = ["Giardino", "Terrazzo vivibile"]
+
+window.onload = (event) => {
+    document.getElementById("comuni").textContent = comunidata.join(', ');
+
+    document.getElementById("tipologia").textContent = tipologiadata.join(', ');
+
+    document.getElementById("altre").textContent = altreprefdata.join(', ');
+}
+
